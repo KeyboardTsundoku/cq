abc53d117ca3c8f5fc29dc19fdf06431ef709a88
Tsundoku-Dev <satyendra.n.sinha@gmail.com>
2016-05-24 22:37:02 +1000

    added css to index

--- make.py
+++ make.py
@@ -6,22 +6,32 @@ import subprocess
 # folders
 rootf = os.path.expanduser('~') + "/Dropbox/cqtheme/"
 jsf = rootf + 'js/'
+cssf = rootf + 'css/'
 
 # files
 html = "index.html"
-js = "index.js"
+js = "cq.js"
+css = "cq.css"
 
-# put index file into root folder
+print("Currently Running => converting index.haml to index.html")
+subprocess.call(['haml', 'index.haml', 'index.html'])
+
+# put index html into root folder
 print("Currently Running => transferring index.html")
-shutil.copy2(html, rootf)
+try:
+  os.remove(rootf + html)
+except OSError:
+  pass
+
+shutil.move(html, rootf)
 
 # convert coffee file to js and put in <root>/js
-print("Currently Running => converting coffee files to index.js")
+print("Currently Running => converting coffee files to " + js)
 cat = subprocess.Popen('cat coffee/*.coffee', shell=True, stdout=subprocess.PIPE)
 tmpIndex = open(js, "w")
 coffee = subprocess.Popen(['coffee', '-c', '-s'], stdin=cat.stdout, stdout=tmpIndex)
 
-print("Currently Running => transferring index.js")
+print("Currently Running => transferring " + js)
 try:
   os.remove(jsf + js)
 except OSError:
@@ -30,6 +40,14 @@ except OSError:
 shutil.move(js, jsf)
 
 # convert scss file to css and put in <root>/css
+print("Currently Running => converting coffee files to " + css)
+subprocess.call(['sass', 'sass/main.sass', css])
 
+# put css file into root folder
+print("Currently Running => transferring " + css)
+try:
+  os.remove(cssf + css)
+except OSError:
+  pass
 
-
+shutil.move(css, cssf)
